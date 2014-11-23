18e31e384beee20da97219023ee4f5c42681da3c
gosick <f56112000@gmail.com>
2014-11-23 14:35:37 +0800

    time fixing of DatabaseSeeder.php

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -21,19 +21,20 @@ class UbikeTableSeeder extends Seeder {
 
 			public function run()
 			{
+				$time = time() + 10;
 				$now = date('Y-m-d H:i:s');
 				//delete users table records
 				// DB::table('users')->delete();
 				DB::table('Ubike')->truncate();
 				//insert some dummy records
 				DB::table('Ubike')->insert(array(
-						array('active'=>'1','remainBikes'=>'10','stationNo'=>'0002','updated_at'=> $now),
-						array('active'=>'1','remainBikes'=>'10','stationNo'=>'0005','updated_at'=> $now),
-						array('active'=>'0','remainBikes'=>'10','stationNo'=>'0006','updated_at'=> $now),
-						array('active'=>'0','remainBikes'=>'10','stationNo'=>'0003','updated_at'=> $now),
-						array('active'=>'1','remainBikes'=>'10','stationNo'=>'0001','updated_at'=> $now),
-						array('active'=>'0','remainBikes'=>'10','stationNo'=>'0008','updated_at'=> $now),
-						array('active'=>'1','remainBikes'=>'10','stationNo'=>'0004','updated_at'=> $now)
+						array('active'=>'1','remainBikes'=>'10','stationNo'=>'0002','created_at'=>$now,'updated_at'=>date('Y-m-d H:i:s', $time)),
+						array('active'=>'1','remainBikes'=>'10','stationNo'=>'0005','created_at'=>$now,'updated_at'=>date('Y-m-d H:i:s', $time)),
+						array('active'=>'0','remainBikes'=>'10','stationNo'=>'0006','created_at'=>$now,'updated_at'=>date('Y-m-d H:i:s', $time)),
+						array('active'=>'0','remainBikes'=>'10','stationNo'=>'0003','created_at'=>$now,'updated_at'=>date('Y-m-d H:i:s', $time)),
+						array('active'=>'1','remainBikes'=>'10','stationNo'=>'0001','created_at'=>$now,'updated_at'=>date('Y-m-d H:i:s', $time)),
+						array('active'=>'0','remainBikes'=>'10','stationNo'=>'0008','created_at'=>$now,'updated_at'=>date('Y-m-d H:i:s', $time)),
+						array('active'=>'1','remainBikes'=>'10','stationNo'=>'0004','created_at'=>$now,'updated_at'=>date('Y-m-d H:i:s', $time))
 
 					));
 			}
